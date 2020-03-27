2876f168cc832280ebe9de76c688306cbfc099a8
junnae <caeses@gmail.com>
2020-03-27 19:18:25 +0100

    Oppdater verdier i journalføring

--- src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/rest/dokarkiv/JournalpostRequest.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/rest/dokarkiv/JournalpostRequest.kt
@@ -6,21 +6,19 @@ data class JournalpostRequest(
         val tema: String = "SYK",
         val bruker: Bruker,
         val avsenderMottaker: AvsenderMottaker,
-        val tittel: String = "Søknad om Refusjon i forbindelse med Corona", //TODO
+        val tittel: String = "Refusjonskrav arbeidsgiverperiode korona",
         val dokumenter: List<Dokument>,
         val sak: Sak = Sak(),
-        val datoMottatt: LocalDate = LocalDate.now() //TODO
+        val datoMottatt: LocalDate = LocalDate.now()
 )
 
 data class Dokument(
-        val brevkode: String = "CORONAREFUSJONSØKNAD", //TODO
-        val dokumentKategori: String = "SOK", //TODO
-        val tittel: String = "Søknad refusjon corona", //TODO
+        val brevkode: String = "refusjonskrav_arbeidsgiverperiode_korona",
+        val tittel: String = "Refusjonskrav arbeidsgiverperiode korona",
         val dokumentVarianter: List<DokumentVariant>
 )
 
 data class DokumentVariant(
-        val filnavn: String = "coronasøknad.pdf", //TODO
         val filtype: String = "PDFA",
         val fysiskDokument: String,
         val variantFormat: String = "ARKIV"
