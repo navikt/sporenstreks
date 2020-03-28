589ddb4f92b2ff07ef709e5c2a2f0af9f95cf301
Morten Laukvik <morten@laukvik.no>
2020-03-28 11:09:11 +0100

    OppgaveKlient

--- src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/rest/oppgave/OppgaveKlient.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/rest/oppgave/OppgaveKlient.kt
@@ -1,4 +1,120 @@
 package no.nav.helse.sporenstreks.integrasjon.rest.oppgave
 
-class OppgaveKlient {
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.JacksonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import no.nav.helse.sporenstreks.integrasjon.rest.sts.STSClient
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+@Component
+class OppgaveKlient constructor (
+        private val url: String, private val stsClient: STSClient
+) {
+
+    private var httpClient = buildClient()
+
+    private fun buildClient(): HttpClient {
+        return HttpClient(Apache) {
+            install(JsonFeature) {
+                serializer = JacksonSerializer {
+                    registerKotlinModule()
+                    registerModule(JavaTimeModule())
+                    configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+                expectSuccess = false
+            }
+        }
+    }
+
+    private suspend fun opprettOppgave(opprettOppgaveRequest: OpprettOppgaveRequest, msgId: String, token: String): OpprettOppgaveResponse {
+        return httpClient.post(url) {
+            contentType(ContentType.Application.Json)
+            this.header("Authorization", "Bearer $token")
+            this.header("X-Correlation-ID", msgId)
+            body = opprettOppgaveRequest
+        }
+    }
+
+    fun mapOppgave(sakId: String, journalpostId: String, aktørId: String, geografiskTilknytning: String): OpprettOppgaveRequest {
+        return OpprettOppgaveRequest(
+                tildeltEnhetsnr = geografiskTilknytning,
+                aktoerId = aktørId,
+                journalpostId = journalpostId,
+                behandlesAvApplikasjon = "FS22",
+                saksreferanse = sakId,
+                beskrivelse = "StrukturertInformasjon",
+                tema = "SYK",
+                oppgavetype = "Robotbehandling",
+                behandlingstype = "",
+                behandlingstema = "refusjonskrav dag 4",
+                aktivDato = LocalDate.now().plusDays(7),
+                prioritet = "NORM"
+        )
+    }
+
+    /**
+     * Når det gjelder Oppgavetype og Behandlingstema så har vi fått tildelt disse:
+     * Oppgavetype = «Robotbehandling»;
+     * Behandlingstema = "refusjonskrav dag 4«;
+     */
+    suspend fun opprettOppgave(
+            sakId: String,
+            journalpostId: String,
+            aktørId: String,
+            geografiskTilknytning: String
+    ): OppgaveResultat {
+        val opprettOppgaveRequest = mapOppgave(sakId, journalpostId, aktørId, geografiskTilknytning)
+        val correlationId =  "123"
+        try {
+            log.info("Oppretter oppgave")
+            return OppgaveResultat(opprettOppgave(opprettOppgaveRequest, correlationId, stsClient.getOidcToken()).id, false)
+        } catch (ex : Exception) {
+            throw OpprettOppgaveException(journalpostId)
+        }
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(STSClient::class.java)
+    }
+
 }
+
+class OpprettOppgaveException(message: String) : Exception(message)
+
+data class OpprettOppgaveRequest(
+        val tildeltEnhetsnr: String? = null,
+        val aktoerId: String? = null,
+        val journalpostId: String? = null,
+        val behandlesAvApplikasjon: String? = null,
+        val saksreferanse: String? = null,
+        val beskrivelse: String? = null,
+        val tema: String? = null,
+        val oppgavetype: String,
+        val behandlingstype: String? = null,
+        val behandlingstema: String? = null,
+        val aktivDato: LocalDate,
+        val fristFerdigstillelse: LocalDate? = null,
+        val prioritet: String
+)
+
+data class OpprettOppgaveResponse(
+        val id: Int
+)
+
+data class OppgaveResultat(
+        val oppgaveId: Int,
+        val duplikat: Boolean
+)
+
