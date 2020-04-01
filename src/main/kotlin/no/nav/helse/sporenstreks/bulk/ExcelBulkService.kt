ded6e9c492b380be78990fb5338cc16a6e7291c6
tuxbear <ole-andre.riga.johansen@nav.no>
2020-04-01 09:16:23 +0200

    Mer nøyaktig feilmeldinger fra parsing

--- src/main/kotlin/no/nav/helse/sporenstreks/bulk/ExcelBulkService.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/bulk/ExcelBulkService.kt
@@ -5,20 +5,20 @@ import no.nav.helse.sporenstreks.db.RefusjonskravRepository
 import no.nav.helse.sporenstreks.domene.Arbeidsgiverperiode
 import no.nav.helse.sporenstreks.domene.Refusjonskrav
 import no.nav.helse.sporenstreks.web.dto.RefusjonskravDto
-import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellType
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFCell
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.slf4j.LoggerFactory
+import org.valiktor.ConstraintViolation
 import java.io.InputStream
-import java.lang.IllegalArgumentException
 import java.time.LocalDate
 import javax.ws.rs.ForbiddenException
 
 
 class ExcelBulkService(private val db: RefusjonskravRepository, private val authorizer: Authorizer) {
+    private val maxRowNum: Int = 5000
     val log = LoggerFactory.getLogger(ExcelBulkService::class.java)
 
     fun processExcelFile(file: InputStream, opprettetAvIdentitetsnummer: String)  {
@@ -27,7 +27,7 @@ class ExcelBulkService(private val db: RefusjonskravRepository, private val auth
         val sheet = workbook.getSheetAt(0);
 
         val refusjonsKrav = HashSet<Refusjonskrav>()
-        val errors = HashSet<ExcelFileRowError>()
+        val errorRows = HashSet<ExcelFileRowError>()
 
         val startDataRow = 11
         var currentDataRow = startDataRow
@@ -37,9 +37,15 @@ class ExcelBulkService(private val db: RefusjonskravRepository, private val auth
             try {
                 val krav = extractRefusjonsKravFromExcelRow(row, opprettetAvIdentitetsnummer)
                 refusjonsKrav.add(krav)
-            } catch (ex: Exception) {
-                errors.add(ExcelFileRowError(
-                    currentDataRow+1,
+            } catch (ex: ForbiddenException) {
+                errorRows.add(ExcelFileRowError(
+                        currentDataRow+1,
+                        "virksomhetsnummer",
+                        "Du har ikke korrekte tilganger for denne virksomheten")
+                )
+            } catch (ex: CellValueExtractionException) {
+                errorRows.add(ExcelFileRowError(
+                        currentDataRow+1,
                         "",
                         ex.message ?: "Ukjent feil")
                 )
@@ -48,13 +54,16 @@ class ExcelBulkService(private val db: RefusjonskravRepository, private val auth
             }
         }
 
-        log.info("Hentet ut ${refusjonsKrav.size} krav og ${errors.size} feil")
-        if (errors.isNotEmpty()) {
-            throw ExcelFileParsinException(errors)
+        log.info("Hentet ut ${refusjonsKrav.size} krav og ${errorRows.size} feil")
+        if (errorRows.isNotEmpty()) {
+            throw ExcelFileParsingException("Det er feil i filen.", errorRows)
+        }
+
+        if (refusjonsKrav.size > maxRowNum) {
+            throw ExcelFileParsingException("Det er for mange rader i filen. Maks er $maxRowNum")
         }
 
         log.info("Lagrer ${refusjonsKrav.size} krav")
-
         // TODO: bruk en spesialisert metode med transaksjon for dette
         refusjonsKrav.forEach { db.insert(it) }
 
@@ -65,12 +74,12 @@ class ExcelBulkService(private val db: RefusjonskravRepository, private val auth
 
     fun extractRefusjonsKravFromExcelRow(row: Row, opprettetAvIdentitetsnummer: String): Refusjonskrav {
         // extract values
-        val identitetsnummer = row.getCell(0).extractCellValueAsString()
-        val virksomhetsNummer = row.getCell(1).extractCellValueAsString()
-        val fom = LocalDate.parse(row.getCell(2).extractCellValueAsString())
-        val tom = LocalDate.parse(row.getCell(3).extractCellValueAsString())
-        val antallDager = row.getCell(4).extractCellValueAsString().toDouble().toInt()
-        val beloep = row.getCell(5).extractCellValueAsString().toDouble()
+        val identitetsnummer = row.extract(0, "identitetsnummer")
+        val virksomhetsNummer = row.extract(1, "virksomhetsnummer")
+        val fom = LocalDate.parse(row.extract(2, "fom"))
+        val tom = LocalDate.parse(row.extract(3, "tom"))
+        val antallDager = row.extract(4, "antall dager").toDouble().toInt()
+        val beloep = row.extract(5, "beløp").toDouble()
 
         // authorize the use
         if (!authorizer.hasAccess(opprettetAvIdentitetsnummer, virksomhetsNummer)) {
@@ -94,27 +103,31 @@ class ExcelBulkService(private val db: RefusjonskravRepository, private val auth
         )
     }
 
-    private fun Cell.extractCellValueAsString(): String {
-        return when (this.cellType) {
-            CellType.BLANK -> ""
-            CellType.ERROR -> "error"
-            CellType._NONE -> "feil i celletypen"
-            CellType.NUMERIC -> (this as XSSFCell).rawValue
-            CellType.STRING -> this.stringCellValue.trim()
-            CellType.FORMULA -> (this as XSSFCell).rawValue
-            CellType.BOOLEAN -> "feil celletype"
-            else -> ""
+    private fun Row.extract(cellNum: Int, columnName: String): String {
+        try {
+            val cell = this.getCell(cellNum)
+            return when (cell.cellType) {
+                CellType.BLANK -> ""
+                CellType.ERROR -> "error"
+                CellType._NONE -> "feil i celletypen"
+                CellType.NUMERIC -> (cell as XSSFCell).rawValue
+                CellType.STRING -> cell.stringCellValue.trim()
+                CellType.FORMULA -> (cell as XSSFCell).rawValue
+                CellType.BOOLEAN -> "feil celletype"
+                else -> ""
+            }
+        } catch (ex: Exception) {
+            throw CellValueExtractionException(columnName, ex)
         }
     }
-
 }
 
-class ExcelFileParsinException(val errors: Set<ExcelFileRowError>) : Exception() {
-
-}
+class ExcelFileParsingException(message: String, val errors: Set<ExcelFileRowError> = emptySet()) : Exception(message)
+class CellValueExtractionException(val columnName: String, cause: Exception) : Exception(cause)
 
 data class ExcelFileRowError(
     val rowNumber: Int,
     val column: String,
-    val message: String
+    val message: String,
+    val validationErrors: Set<ConstraintViolation> = emptySet()
 )
