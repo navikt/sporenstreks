ff0764259ccb99786e796b5e3bb4c8041060b790
MajaHelene <maja.helene.pedersen@nav.no>
2020-09-29 16:40:08 +0200

    fiks tester

--- src/test/kotlin/no/nav/helse/sporenstreks/web/integration/HealthCheckTests.kt
+++ src/test/kotlin/no/nav/helse/sporenstreks/web/integration/HealthCheckTests.kt
@@ -1,11 +1,16 @@
 package no.nav.helse.sporenstreks.web.integration
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.server.testing.handleRequest
 import io.ktor.util.KtorExperimentalAPI
+import no.nav.helse.arbeidsgiver.kubernetes.KubernetesProbeManager
+import no.nav.helse.arbeidsgiver.kubernetes.LivenessComponent
+import no.nav.helse.arbeidsgiver.kubernetes.ReadynessComponent
 import no.nav.helse.sporenstreks.auth.StaticMockAuthRepo
+import no.nav.helse.sporenstreks.web.getAllOfType
 import no.nav.helse.sporenstreks.web.sporenstreksModule
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -21,6 +26,8 @@ class HealthCheckTests : ControllerIntegrationTestBase() {
             get<StaticMockAuthRepo>().failSelfCheck = true
 
         }) {
+            val probeManager = application.get<KubernetesProbeManager>()
+            probeManager.registerReadynessComponent(application.get<StaticMockAuthRepo>())
             handleRequest(HttpMethod.Get, "/healthcheck") {
             }.apply {
                 assertEquals(HttpStatusCode.InternalServerError, response.status())
@@ -36,6 +43,8 @@ class HealthCheckTests : ControllerIntegrationTestBase() {
             get<StaticMockAuthRepo>().failSelfCheck = false
 
         }) {
+            val probeManager = application.get<KubernetesProbeManager>()
+            probeManager.registerReadynessComponent(StaticMockAuthRepo(application.get<ObjectMapper>()))
             handleRequest(HttpMethod.Get, "/healthcheck") {
             }.apply {
                 assertEquals(HttpStatusCode.OK, response.status())
