395a73ad8613cdf16b7351dcc2902b9fd66979d5
tuxbear <ole-andre.riga.johansen@nav.no>
2020-10-07 14:01:26 +0200

    Tester for oppgaveservice

--- src/test/kotlin/no/nav/helse/TestData.kt
+++ src/test/kotlin/no/nav/helse/TestData.kt
@@ -1,8 +1,20 @@
 package no.nav.helse
 
+import no.nav.helse.sporenstreks.domene.Arbeidsgiverperiode
+import no.nav.helse.sporenstreks.domene.Refusjonskrav
+import no.nav.helse.sporenstreks.domene.RefusjonskravStatus
+import java.time.LocalDate
+import java.time.LocalDate.of
+
 object TestData {
     val validIdentitetsnummer = "20015001543"
     val notValidIdentitetsnummer = "50012001987"
     val validOrgNr = "123456785"
     val notValidOrgNr = "123456789"
+    val gyldigKrav = Refusjonskrav(
+            validIdentitetsnummer,
+            validOrgNr,
+            setOf(Arbeidsgiverperiode(of(2020, 4,4), of(2020, 4,10), 2, 1000.0)),
+            RefusjonskravStatus.MOTTATT
+    )
 }
