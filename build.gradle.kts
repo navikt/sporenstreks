56a38d55deda8048e0b10b6a0398e1e059b061b5
Ole-André Riga-Johansen <ole-andre.riga.johansen@nav.no>
2020-11-26 11:49:53 +0100

    Bruker NAVs felles loggfilter (#37)
    * Bruker NAVs felles loggfilter
    
    Dette erstatter FNR med ****** i loggene
    
    * Felles loggfilter
    
    * Snyk

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val mockKVersion = "1.9.3"
 val tokenSupportVersion = "1.3.1"
 val koinVersion = "2.0.1"
 val valiktorVersion = "0.10.0"
-val cxfVersion = "3.4.0"
+val cxfVersion = "3.4.1"
 val jaxwsVersion = "2.3.1"
 val jaxwsToolsVersion = "2.3.3"
 val apachePoiVersion = "4.1.2"
@@ -78,6 +78,7 @@ dependencies {
     implementation("io.netty:netty-codec-http:4.1.53.Final") // overstyrer transiente 4.1.51.Final
     implementation("org.glassfish.jersey.media:jersey-media-jaxb:2.31") // overstyrer transiente 2.30.1
     implementation("junit:junit:4.13.1") // overstyrer transiente 4.12
+    implementation("org.apache.httpcomponents:httpclient:4.5.13") // overstyrer transiente 4.5.6 via ktor-client-apache
     implementation("com.google.guava:guava:30.0-jre") // overstyrer transiente 29.0-jre
     // -- end snyk fixes
 
@@ -114,6 +115,8 @@ dependencies {
     implementation("org.koin:koin-ktor:$koinVersion")
     implementation("no.nav.security:token-validation-ktor:$tokenSupportVersion")
     implementation("no.nav.security:token-validation-test-support:$tokenSupportVersion")
+
+    implementation("no.nav.common:log:2.2020.10.15_11.43-b1f02e7bd6ae")
 
     implementation(kotlin("stdlib"))
 
