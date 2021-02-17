1f098076cd497c0cc128dc13e83533ebe3b7376e
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-02-17 11:32:44 +0100

    gradle ver --> 6.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,13 +99,9 @@ dependencies {
     }
     // SNYK-fikser - Disse kan fjernes etterhver som våre avhengigheter oppdaterer sine versjoner
     // Forsøk å fjerne en og en og kjør snyk test --configuration-matching=runtimeClasspath
-    //implementation("commons-collections:commons-collections:3.2.2") // overstyrer transiente 3.2.1
     implementation("io.netty:netty-codec:4.1.59.Final") // overstyrer transiente 4.1.44
     implementation("io.netty:netty-codec-http:4.1.59.Final") // overstyrer transiente 4.1.51.Final
     implementation("org.glassfish.jersey.media:jersey-media-jaxb:2.31") // overstyrer transiente 2.30.1
-   // implementation("junit:junit:4.13.1") // overstyrer transiente 4.12
-   // implementation("org.apache.httpcomponents:httpclient:4.5.13") // overstyrer transiente 4.5.6 via ktor-client-apache
-    //implementation("com.google.guava:guava:30.1-jre") // overstyrer transiente 29.0-jre
     implementation("org.eclipse.jetty:jetty-server:11.0.0")
     // -- end snyk fixes
 
@@ -127,16 +123,12 @@ dependencies {
     implementation("org.apache.poi:poi:$apachePoiVersion")
     implementation("org.apache.poi:poi-ooxml:$apachePoiVersion")
 
-   // implementation("javax.xml.ws:jaxws-api:$jaxwsVersion") -- ikke brukt 1
     implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
-   // implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion") -- ikke brukt 3
-   // implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion") -- ikke brukt 4
     implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
     //implementation("org.apache.ws.xmlschema:xmlschema-core:2.2.4") // Force newer version of XMLSchema to fix illegal reflective access warning
     implementation("com.sun.xml.ws:jaxws-tools:$jaxwsToolsVersion") {
         exclude(group = "com.sun.xml.ws", module = "policy")
     }
-  //  implementation("com.sun.activation:javax.activation:1.2.0") -- ikke brukt 5
 
     implementation("org.koin:koin-core:$koinVersion")
     implementation("org.koin:koin-ktor:$koinVersion")
@@ -147,13 +139,8 @@ dependencies {
 
     implementation(kotlin("stdlib"))
 
-    implementation("org.slf4j:slf4j-api:1.7.30")
+    api("org.slf4j:slf4j-api:1.7.30")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    //implementation("ch.qos.logback.contrib:logback-jackson:$logback_contrib_version") -- ikke brukt 8
-    //implementation("ch.qos.logback.contrib:logback-json-classic:$logback_contrib_version") -- ikke brukt 2
-  //  implementation("net.logstash.logback:logstash-logback-encoder:6.6") -- ikke brukt 6
-    //implementation("org.codehaus.janino:janino:3.0.6") -- ikke brukt 7
-
     implementation("no.nav.tjenestespesifikasjoner:altinn-correspondence-agency-external-basic:1.2019.09.25-00.21-49b69f0625e0")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
@@ -255,5 +242,5 @@ task<Test>("slowTests") {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.0.1"
+    gradleVersion = "6.8.2"
 }
