fed7921d90c9de7bbd9e09bfdb3b09cb5bd03637
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-02-18 11:37:43 +0100

    felles backend kan ikke lastes ned from jcenter og mavencenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -189,8 +189,16 @@ java {
 }
 
 repositories {
-    jcenter()
-    mavenCentral()
+    jcenter{
+        content {
+            excludeGroup("no.nav.helsearbeidsgiver")
+        }
+    }
+    mavenCentral{
+        content {
+            excludeGroup("no.nav.helsearbeidsgiver")
+        }
+    }
     maven("https://kotlin.bintray.com/ktor")
     maven {
         credentials {
