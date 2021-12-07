7d7671adb66b6f18e5c8754094549485095948d5
Gustav Berggren <gustav.berggren@nav.no>
2021-12-07 15:30:50 +0100

    Tester for ArgeidsgiverBetalerForDager

---
+++ src/test/kotlin/no/nav/helse/sporenstreks/web/dto/validation/RefusjonsDagerConstraintTest.kt
@@ -0,0 +1,100 @@
+package no.nav.helse.sporenstreks.web.dto.validation
+
+import no.nav.helse.sporenstreks.domene.Arbeidsgiverperiode
+import org.junit.jupiter.api.Test
+import org.valiktor.validate
+import java.time.LocalDate
+
+class RefusjonsDagerConstraintTest {
+
+    data class RefusjonskravDtoTestClass(val perioder: Set<Arbeidsgiverperiode>)
+
+    @Test
+    fun `Ugyldige perioder før nedstenging`() {
+        val testRefusjon = RefusjonskravDtoTestClass(
+            perioder = setOf(
+                Arbeidsgiverperiode(
+                    LocalDate.of(2019, 5, 1),
+                    LocalDate.of(2019, 5, 6),
+                    4,
+                    2590.8
+                ),
+                Arbeidsgiverperiode(
+                    LocalDate.of(2019, 5, 8),
+                    LocalDate.of(2019, 5, 15),
+                    5,
+                    2590.8
+                ),
+            )
+        )
+        validationShouldFailFor(RefusjonskravDtoTestClass::perioder) {
+            validate(testRefusjon) {
+                validate(RefusjonskravDtoTestClass::perioder).arbeidsgiverBetalerForDager(LocalDate.of(2020, 3, 16))
+            }
+        }
+    }
+
+    @Test
+    fun `Gyldige perioder før 1 oktober`() {
+        val testRefusjon = RefusjonskravDtoTestClass(
+            perioder = setOf(
+                Arbeidsgiverperiode(
+                    LocalDate.of(2021, 5, 1),
+                    LocalDate.of(2021, 5, 6),
+                    4,
+                    2590.8
+                ),
+                Arbeidsgiverperiode(
+                    LocalDate.of(2021, 5, 8),
+                    LocalDate.of(2021, 5, 15),
+                    5,
+                    2590.8
+                ),
+            )
+        )
+
+        validate(testRefusjon) {
+            validate(RefusjonskravDtoTestClass::perioder).arbeidsgiverBetalerForDager(LocalDate.of(2020, 3, 16))
+        }
+    }
+
+    @Test
+    fun `Gyldige perioder etter 1 desember`() {
+        val testRefusjon = RefusjonskravDtoTestClass(
+            perioder = setOf(
+                Arbeidsgiverperiode(
+                    LocalDate.of(2021, 12, 1),
+                    LocalDate.of(2021, 12, 6),
+                    4,
+                    2590.8
+                ),
+                Arbeidsgiverperiode(
+                    LocalDate.of(2021, 12, 8),
+                    LocalDate.of(2021, 12, 15),
+                    5,
+                    2590.8
+                ),
+            )
+        )
+        validate(testRefusjon) {
+            validate(RefusjonskravDtoTestClass::perioder).arbeidsgiverBetalerForDager(LocalDate.of(2020, 3, 6))
+        }
+    }
+
+    @Test
+    fun `Gyldig periode som starter før 1 desember`() {
+        val testRefusjon = RefusjonskravDtoTestClass(
+            perioder = setOf(
+                Arbeidsgiverperiode(
+                    LocalDate.of(2021, 11, 22),
+                    LocalDate.of(2021, 12, 2),
+                    1,
+                    2590.8
+                )
+            )
+        )
+        validate(testRefusjon) {
+            validate(RefusjonskravDtoTestClass::perioder).arbeidsgiverBetalerForDager(LocalDate.of(2020, 3, 6))
+        }
+    }
+}
