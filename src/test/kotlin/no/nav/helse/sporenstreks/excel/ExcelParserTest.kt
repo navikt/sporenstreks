b332968f0f415c1316e715522e1d4f376ef59b82
Gustav Berggren <gustav.berggren@nav.no>
2021-12-16 16:32:33 +0100

    POH-1146: Endrer feilende valideringstest av excel ark

--- src/test/kotlin/no/nav/helse/sporenstreks/excel/ExcelParserTest.kt
+++ src/test/kotlin/no/nav/helse/sporenstreks/excel/ExcelParserTest.kt
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
-internal class ExcelParserTest  {
+internal class ExcelParserTest {
 
     fun suspendableTest(block: suspend CoroutineScope.() -> Unit) {
         runBlocking {
@@ -58,11 +58,13 @@ internal class ExcelParserTest  {
 
         val rowErrors = result.errors.groupBy { it.rowNumber }
 
-        assertThat(rowErrors[12]?.size).isEqualTo(1)
-        assertThat(rowErrors[12]?.get(0)?.column).isEqualTo("Fødselsnummer")
+        assertThat(rowErrors[12]?.size).isEqualTo(2)
+        assertThat(rowErrors[12]?.get(0)?.column).isEqualTo("Arbeidsgiverperioden (fom+tom)")
+        assertThat(rowErrors[12]?.get(1)?.column).isEqualTo("Fødselsnummer")
 
-        assertThat(rowErrors[13]?.size).isEqualTo(1)
-        assertThat(rowErrors[13]?.get(0)?.column).isEqualTo("Virksomhetsnummer")
+        assertThat(rowErrors[13]?.size).isEqualTo(2)
+        assertThat(rowErrors[13]?.get(0)?.column).isEqualTo("Arbeidsgiverperioden (fom+tom)")
+        assertThat(rowErrors[13]?.get(1)?.column).isEqualTo("Virksomhetsnummer")
 
         assertThat(rowErrors[14]?.size).isEqualTo(1)
         assertThat(rowErrors[14]?.get(0)?.column).isEqualTo("Fra og med")
@@ -70,9 +72,10 @@ internal class ExcelParserTest  {
         assertThat(rowErrors[15]?.size).isEqualTo(1)
         assertThat(rowErrors[15]?.get(0)?.column).isEqualTo("Til og med")
 
-        assertThat(rowErrors[16]?.size).isEqualTo(2)
+        assertThat(rowErrors[16]?.size).isEqualTo(3)
         assertThat(rowErrors[16]?.get(0)?.column).isEqualTo("Arbeidsgiverperioden (fom+tom)")
         assertThat(rowErrors[16]?.get(1)?.column).isEqualTo("Arbeidsgiverperioden (fom+tom)")
+        assertThat(rowErrors[16]?.get(2)?.column).isEqualTo("Arbeidsgiverperioden (fom+tom)")
 
         assertThat(rowErrors[17]?.size).isEqualTo(1)
         assertThat(rowErrors[17]?.get(0)?.column).isEqualTo("Beløp")
@@ -94,11 +97,7 @@ internal class ExcelParserTest  {
         val workbook: Workbook = XSSFWorkbook(validTarrifendringFile)
 
         val result = ExcelParser(authorizerMock, aaregMock).parseAndValidateExcelContent(workbook, TestData.validIdentitetsnummer)
-
-        println(result)
-
         assertThat(result.refusjonskrav.size).isEqualTo(11)
         assertThat(result.refusjonskrav.all { it.tariffEndring })
     }
-
 }
