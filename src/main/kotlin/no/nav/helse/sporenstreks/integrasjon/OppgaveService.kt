160d167df19bcb40eaaa461255311cbd6075e51d
Gustav Berggren <gustav.berggren@nav.no>
2021-12-21 10:26:22 +0100

    setter riktig oppgavetype ved tariffendring

--- src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/OppgaveService.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/integrasjon/OppgaveService.kt
@@ -24,7 +24,7 @@ class OppgaveService(private val oppgaveKlient: OppgaveKlient, private val om: O
     }
 
     private fun mapOppgave(journalpostId: String, aktørId: String, beskrivelse: String, tariffEndring: Boolean): OpprettOppgaveRequest {
-        val oppgaveType = if (!tariffEndring) "ROB_BEH" else "REF_BEH"
+        val oppgaveType = if (!tariffEndring) "ROB_BEH" else "VUR_KONS_YTE"
         val behandlingsTema = if (!tariffEndring) "ab0456" else "ab0433"
 
         return OpprettOppgaveRequest(
