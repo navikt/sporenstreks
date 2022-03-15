becac0d58881b0e1f7aacb14b25df4f71b2e26fd
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-03-15 16:50:33 +0100

    Legger til flere tester for sammenslåing

--- src/test/kotlin/no/nav/helse/sporenstreks/web/dto/AaregConstraintsTest.kt
+++ src/test/kotlin/no/nav/helse/sporenstreks/web/dto/AaregConstraintsTest.kt
@@ -36,190 +36,187 @@ internal class AaregConstraintsTest {
         every { LocalDate.now() } returns LocalDate.parse("2022-01-20")
     }
 
-    class AaregConstraintsKtTest {
-        @Test
-        @Disabled
-        fun `Rådata fra aareg (Brukes for å feilsøke med respons fra AA-reg)`() {
-            val om = ObjectMapper()
-            om.registerModule(KotlinModule())
-            om.registerModule(Jdk8Module())
-            om.registerModule(JavaTimeModule())
-            om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            om.configure(SerializationFeature.INDENT_OUTPUT, true)
-            om.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-            om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    @Test
+    @Disabled
+    fun `Rådata fra aareg (Brukes for å feilsøke med respons fra AA-reg)`() {
+        val om = ObjectMapper()
+        om.registerModule(KotlinModule())
+        om.registerModule(Jdk8Module())
+        om.registerModule(JavaTimeModule())
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        om.configure(SerializationFeature.INDENT_OUTPUT, true)
+        om.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-            om.setDefaultPrettyPrinter(
-                DefaultPrettyPrinter().apply {
-                    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
-                    indentObjectsWith(DefaultIndenter("  ", "\n"))
-                }
+        om.setDefaultPrettyPrinter(
+            DefaultPrettyPrinter().apply {
+                indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                indentObjectsWith(DefaultIndenter("  ", "\n"))
+            }
+        )
+
+        // Legg aareg JSON-respons i src/test/resources/aareg.json
+        val aaregFile = "aareg.json".loadFromResources()
+        val arbeidsforhold = om.readValue<List<Arbeidsforhold>>(aaregFile)
+            // Legg inn organisasjonsnummer
+            .filter { it.arbeidsgiver.organisasjonsnummer == "XXXXXXXX" }
+
+        // Endre til perioden kravet gjelder
+        val arbeidsgiverPeriode = Arbeidsgiverperiode(
+            LocalDate.of(2021, 1, 15),
+            LocalDate.of(2021, 1, 20),
+            4,
+            beloep = 2.0
+        )
+
+        val refusjonskravDto = RefusjonskravDto(
+            TestData.validIdentitetsnummer,
+            TestData.validOrgNr,
+            setOf(
+                arbeidsgiverPeriode
             )
+        )
+        refusjonskravDto.validate(arbeidsforhold)
+    }
 
-            // Legg aareg JSON-respons i src/test/resources/aareg.json
-            val aaregFile = "aareg.json".loadFromResources()
-            val arbeidsforhold = om.readValue<List<Arbeidsforhold>>(aaregFile)
-                // Legg inn organisasjonsnummer
-                .filter { it.arbeidsgiver.organisasjonsnummer == "XXXXXXXX" }
-
-            // Endre til perioden kravet gjelder
-            val arbeidsgiverPeriode = Arbeidsgiverperiode(
-                LocalDate.of(2021, 1, 15),
-                LocalDate.of(2021, 1, 20),
-                4,
-                beloep = 2.0
-            )
-
-            val refusjonskravDto = RefusjonskravDto(
-                TestData.validIdentitetsnummer,
-                TestData.validOrgNr,
-                setOf(
-                    arbeidsgiverPeriode
-                )
-            )
-            refusjonskravDto.validate(arbeidsforhold)
-        }
-
-        @Test
-        fun `Gyldig arbeidsforhold og gyldig krav`() {
-            val arbeidsgiver = Arbeidsgiver("AS", "123456785")
-            val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
-            val arbeidsForhold = listOf(
-                Arbeidsforhold(
-                    arbeidsgiver,
-                    opplysningspliktig,
-                    emptyList(),
-                    Ansettelsesperiode(
-                        Periode(
-                            of(2021, 1, 1),
-                            null
-                        )
-                    ),
-                    LocalDateTime.now()
-                )
-            )
-
-            val refusjonskravDto = RefusjonskravDto(
-                TestData.validIdentitetsnummer,
-                TestData.validOrgNr,
-                setOf(
-                    Arbeidsgiverperiode(
-                        of(2022, 1, 1),
-                        of(2022, 1, 7),
-                        2, 2.3
+    @Test
+    fun `Gyldig arbeidsforhold og gyldig krav`() {
+        val arbeidsgiver = Arbeidsgiver("AS", "123456785")
+        val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
+        val arbeidsForhold = listOf(
+            Arbeidsforhold(
+                arbeidsgiver,
+                opplysningspliktig,
+                emptyList(),
+                Ansettelsesperiode(
+                    Periode(
+                        of(2021, 1, 1),
+                        null
                     )
+                ),
+                LocalDateTime.now()
+            )
+        )
+
+        RefusjonskravDto(
+            TestData.validIdentitetsnummer,
+            TestData.validOrgNr,
+            setOf(
+                Arbeidsgiverperiode(
+                    of(2022, 1, 1),
+                    of(2022, 1, 10),
+                    2, 2.3
                 )
             )
+        ).validate(arbeidsForhold)
+    }
+
+    @Test
+    fun `Ikke aktivt arbeidsforhold i krav perioden`() {
+        val arbeidsgiver = Arbeidsgiver("AS", "123456785")
+        val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
+        val arbeidsForhold = listOf(
+            Arbeidsforhold(
+                arbeidsgiver,
+                opplysningspliktig,
+                emptyList(),
+                Ansettelsesperiode(
+                    Periode(
+                        of(2021, 1, 1),
+                        of(2021, 1, 3),
+                    )
+                ),
+                LocalDateTime.now()
+            )
+        )
+
+        val refusjonskravDto = RefusjonskravDto(
+            TestData.validIdentitetsnummer,
+            TestData.validOrgNr,
+            setOf(
+                Arbeidsgiverperiode(
+                    of(2022, 1, 1),
+                    of(2022, 1, 7),
+                    2, 2.3
+                )
+            )
+        )
+        Assertions.assertThatExceptionOfType(ConstraintViolationException::class.java).isThrownBy {
             refusjonskravDto.validate(arbeidsForhold)
-        }
-
-        @Test
-        fun `Ikke aktivt arbeidsforhold i krav perioden`() {
-            val arbeidsgiver = Arbeidsgiver("AS", "123456785")
-            val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
-            val arbeidsForhold = listOf(
-                Arbeidsforhold(
-                    arbeidsgiver,
-                    opplysningspliktig,
-                    emptyList(),
-                    Ansettelsesperiode(
-                        Periode(
-                            of(2021, 1, 1),
-                            of(2021, 1, 3),
-                        )
-                    ),
-                    LocalDateTime.now()
-                )
-            )
-
-            val refusjonskravDto = RefusjonskravDto(
-                TestData.validIdentitetsnummer,
-                TestData.validOrgNr,
-                setOf(
-                    Arbeidsgiverperiode(
-                        of(2022, 1, 1),
-                        of(2022, 1, 7),
-                        2, 2.3
-                    )
-                )
-            )
-            Assertions.assertThatExceptionOfType(ConstraintViolationException::class.java).isThrownBy {
-                refusjonskravDto.validate(arbeidsForhold)
-            }
-        }
-
-        @Test
-        fun `Ikke aktivt arbeidsforhold i denne organisasjonen`() {
-            val arbeidsgiver = Arbeidsgiver("AS", "810007842")
-            val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
-            val arbeidsForhold = listOf(
-                Arbeidsforhold(
-                    arbeidsgiver,
-                    opplysningspliktig,
-                    emptyList(),
-                    Ansettelsesperiode(
-                        Periode(
-                            of(2021, 1, 1),
-                            null
-                        )
-                    ),
-                    LocalDateTime.now()
-                )
-            )
-
-            val refusjonskravDto = RefusjonskravDto(
-                TestData.validIdentitetsnummer,
-                TestData.validOrgNr,
-                setOf(
-                    Arbeidsgiverperiode(
-                        of(2022, 1, 1),
-                        of(2022, 1, 7),
-                        2, 2.3
-                    )
-                )
-            )
-            Assertions.assertThatExceptionOfType(ConstraintViolationException::class.java).isThrownBy {
-                refusjonskravDto.validate(arbeidsForhold)
-            }
         }
     }
 
     @Test
-    fun `merge fragmented periods`() {
-        assertThat(
-            slåSammenPerioder(
-                listOf(
-                    // skal ble merget til 1 periode fra 1.1.21 til 28.2.21
-                    Periode(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 29)),
-                    Periode(LocalDate.of(2021, 2, 1), LocalDate.of(2021, 2, 13)),
-                    Periode(LocalDate.of(2021, 2, 15), LocalDate.of(2021, 2, 28)),
+    fun `Ikke aktivt arbeidsforhold i denne organisasjonen`() {
+        val arbeidsgiver = Arbeidsgiver("AS", "810007842")
+        val opplysningspliktig = Opplysningspliktig("AS", "1212121212")
+        val arbeidsForhold = listOf(
+            Arbeidsforhold(
+                arbeidsgiver,
+                opplysningspliktig,
+                emptyList(),
+                Ansettelsesperiode(
+                    Periode(
+                        of(2021, 1, 1),
+                        null
+                    )
+                ),
+                LocalDateTime.now()
+            )
+        )
 
-                    // skal bli merget til 1
-                    Periode(LocalDate.of(2021, 3, 20), LocalDate.of(2021, 3, 31)),
-                    Periode(LocalDate.of(2021, 4, 2), LocalDate.of(2021, 4, 30)),
-
-                    // skal bli merget til 1
-                    Periode(LocalDate.of(2021, 7, 1), LocalDate.of(2021, 8, 30)),
-                    Periode(LocalDate.of(2021, 9, 1), null),
+        val refusjonskravDto = RefusjonskravDto(
+            TestData.validIdentitetsnummer,
+            TestData.validOrgNr,
+            setOf(
+                Arbeidsgiverperiode(
+                    of(2022, 1, 1),
+                    of(2022, 1, 7),
+                    2, 2.3
                 )
             )
-        ).hasSize(3)
-
-        assertThat(
-            slåSammenPerioder(
-                listOf(
-                    Periode(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 29)),
-                    Periode(LocalDate.of(2021, 9, 1), null),
-                )
-            )
-        ).hasSize(2)
-
-        assertThat(
-            slåSammenPerioder(
-                listOf(
-                    Periode(LocalDate.of(2021, 9, 1), null),
-                )
-            )
-        ).hasSize(1)
+        )
+        Assertions.assertThatExceptionOfType(ConstraintViolationException::class.java).isThrownBy {
+            refusjonskravDto.validate(arbeidsForhold)
+        }
     }
+}
+
+@Test
+fun `merge fragmented periods`() {
+    assertThat(
+        slåSammenPerioder(
+            listOf(
+                // skal ble merget til 1 periode fra 1.1.21 til 28.2.21
+                Periode(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 29)),
+                Periode(LocalDate.of(2021, 2, 1), LocalDate.of(2021, 2, 13)),
+                Periode(LocalDate.of(2021, 2, 15), LocalDate.of(2021, 2, 28)),
+
+                // skal bli merget til 1
+                Periode(LocalDate.of(2021, 3, 20), LocalDate.of(2021, 3, 31)),
+                Periode(LocalDate.of(2021, 4, 2), LocalDate.of(2021, 4, 30)),
+
+                // skal bli merget til 1
+                Periode(LocalDate.of(2021, 7, 1), LocalDate.of(2021, 8, 30)),
+                Periode(LocalDate.of(2021, 9, 1), null),
+            )
+        )
+    ).hasSize(3)
+
+    assertThat(
+        slåSammenPerioder(
+            listOf(
+                Periode(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 29)),
+                Periode(LocalDate.of(2021, 9, 1), null),
+            )
+        )
+    ).hasSize(2)
+
+    assertThat(
+        slåSammenPerioder(
+            listOf(
+                Periode(LocalDate.of(2021, 9, 1), null),
+            )
+        )
+    ).hasSize(1)
 }
