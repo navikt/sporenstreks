d0ba109682bf447bbac2ae853585fcd7a903050b
Gustav Berggren <gustav.berggren@nav.no>
2022-05-12 17:04:39 +0200

    gjør testene grønne

--- src/main/kotlin/no/nav/helse/sporenstreks/web/dto/validation/AaregConstraints.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/web/dto/validation/AaregConstraints.kt
@@ -17,22 +17,29 @@ const val MAKS_DAGER_OPPHOLD = 3L
 fun <E> Validator<E>.Property<Iterable<Arbeidsgiverperiode>?>.måHaAktivtArbeidsforhold(
     refusjonskrav: RefusjonskravDto,
     arbeidsforhold: List<Arbeidsforhold>?
+) =
+    måHaAktivtArbeidsforholdEnkel(refusjonskrav.perioder, refusjonskrav.virksomhetsnummer, arbeidsforhold)
+
+fun <E> Validator<E>.Property<Iterable<Arbeidsgiverperiode>?>.måHaAktivtArbeidsforholdEnkel(
+    arbeidsgiverPerioder: Set<Arbeidsgiverperiode>,
+    virksomhet: String,
+    arbeidsforhold: List<Arbeidsforhold>?
 ) = this.validate(ArbeidsforholdConstraint()) {
     val ansattPerioder = arbeidsforhold!!
-        .filter { it.arbeidsgiver.organisasjonsnummer == refusjonskrav.virksomhetsnummer }
+        .filter { it.arbeidsgiver.organisasjonsnummer == virksomhet }
         .map { it.ansettelsesperiode.periode }
         .sortedWith(compareBy(Periode::fom, Periode::tom))
 
     val sammenslåttePerioder = slåSammenPerioder(ansattPerioder)
 
-    refusjonskrav.perioder
+    arbeidsgiverPerioder
         .all { periode -> sammenslåttePerioder.encloses(Range.open(periode.fom, periode.tom)) }
 }
 
 fun slåSammenPerioder(perioder: List<Periode>): RangeSet<LocalDate> {
     val sammenslåttePerioder: RangeSet<LocalDate> = TreeRangeSet.create()
-    perioder.forEach { periode ->
-        val fomTidligst = periode.fom!!.minusDays(MAKS_DAGER_OPPHOLD)
+    perioder.sortedWith(compareBy(Periode::fom, Periode::tom)).forEach { periode ->
+        val fomTidligst = if (periode.fom!!.year > 0) periode.fom!!.minusDays(MAKS_DAGER_OPPHOLD) else LocalDate.of(0, 1, 1)
         val overlappendePeriode = sammenslåttePerioder.intersects(Range.atLeast(fomTidligst))
         val f = if (overlappendePeriode) { fomTidligst } else periode.fom
         if (periode.tom == null) sammenslåttePerioder.add(Range.atLeast(f))
