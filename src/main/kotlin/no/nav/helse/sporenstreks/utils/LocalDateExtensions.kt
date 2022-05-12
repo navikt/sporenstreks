633e39a62d47444fbd0109ffa148b9f00c619993
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-05-12 20:03:42 +0200

    Forsøk på omskrivning av slåSammenPerioder

--- src/main/kotlin/no/nav/helse/sporenstreks/utils/LocalDateExtensions.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/utils/LocalDateExtensions.kt
@@ -3,7 +3,7 @@ package no.nav.helse.sporenstreks.utils
 import java.time.LocalDate
 
 fun LocalDate.isAfterOrEqual(other: LocalDate): Boolean =
-    this.isEqual(other)|| this.isAfter(other)
+    this.isEqual(other) || this.isAfter(other)
 
 fun LocalDate.isBeforeOrEqual(other: LocalDate): Boolean =
     this.isEqual(other) || this.isBefore(other)
