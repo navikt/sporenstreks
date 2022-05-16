67ba1b2fa6f7c34189eefe21a28eadb890f00211
Gustav Berggren <gustav.berggren@nav.no>
2022-05-16 16:35:17 +0200

    Legg til flere tester og skriv om Valideringen

--- src/main/kotlin/no/nav/helse/sporenstreks/web/dto/validation/AaregConstraints.kt
+++ src/main/kotlin/no/nav/helse/sporenstreks/web/dto/validation/AaregConstraints.kt
@@ -6,7 +6,6 @@ import com.google.common.collect.TreeRangeSet
 import no.nav.helse.arbeidsgiver.integrasjoner.aareg.Arbeidsforhold
 import no.nav.helse.arbeidsgiver.integrasjoner.aareg.Periode
 import no.nav.helse.sporenstreks.domene.Arbeidsgiverperiode
-import no.nav.helse.sporenstreks.web.dto.RefusjonskravDto
 import org.valiktor.Validator
 import java.time.LocalDate
 
@@ -14,16 +13,10 @@ class ArbeidsforholdConstraint : CustomConstraint
 
 const val MAKS_DAGER_OPPHOLD = 3L
 
-fun <E> Validator<E>.Property<Iterable<Arbeidsgiverperiode>?>.måHaAktivtArbeidsforhold(
-    refusjonskrav: RefusjonskravDto,
-    arbeidsforhold: List<Arbeidsforhold>?
-) =
-    måHaAktivtArbeidsforholdEnkel(refusjonskrav.perioder, refusjonskrav.virksomhetsnummer, arbeidsforhold)
-
-fun <E> Validator<E>.Property<Iterable<Arbeidsgiverperiode>?>.måHaAktivtArbeidsforholdEnkel(
-    arbeidsgiverPerioder: Set<Arbeidsgiverperiode>,
+fun <E> Validator<E>.Property<LocalDate?>.måHaAktivtArbeidsforhold(
+    agp: Arbeidsgiverperiode,
     virksomhet: String,
-    arbeidsforhold: List<Arbeidsforhold>?
+    arbeidsforhold: List<Arbeidsforhold>
 ) = this.validate(ArbeidsforholdConstraint()) {
     val ansattPerioder = arbeidsforhold!!
         .filter { it.arbeidsgiver.organisasjonsnummer == virksomhet }
@@ -32,14 +25,12 @@ fun <E> Validator<E>.Property<Iterable<Arbeidsgiverperiode>?>.måHaAktivtArbeids
 
     val sammenslåttePerioder = slåSammenPerioder(ansattPerioder)
 
-    arbeidsgiverPerioder
-        .all { periode -> sammenslåttePerioder.encloses(Range.open(periode.fom, periode.tom)) }
+    return@validate sammenslåttePerioder.encloses(Range.open(agp.fom, agp.tom))
 }
 
 fun slåSammenPerioder(perioder: List<Periode>): RangeSet<LocalDate> {
     val sammenslåttePerioder: RangeSet<LocalDate> = TreeRangeSet.create()
-    perioder.sortedWith(compareBy(Periode::fom, Periode::tom)).forEach { periode ->
-        val r = periodeToRange(periode)
+    perioder.sortedWith(compareBy(Periode::fom, Periode::tom)).map { periodeToRange(it) }.forEach { r ->
         if (sammenslåttePerioder.contains(r.lowerEndpoint().minusDays(3))) {
             sammenslåttePerioder.add(Range.closed(r.lowerEndpoint().minusDays(3), r.upperEndpoint()))
         } else sammenslåttePerioder.add(r)
